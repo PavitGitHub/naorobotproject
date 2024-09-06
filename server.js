55063f36323b5784fff650f0d55bd1104a5becaa
Joel Leifer <jleifer@student.unimelb.edu.au>
2024-09-06 16:32:07 +1000

    added serverportforwarding on modem, added get request page with drumpad style post request inputs

--- server.js
+++ server.js
@@ -14,18 +14,18 @@ app.post('/upload', upload.single('file'), (req, res) => {
   res.status(200).send('File uploaded successfully.');
 });
 
-app.get('/test_python_27_input', (req, res) => {
+app.post('/test_python_27_input', (req, res) => {
   nao.feedLine("test_input_from_nodejs()");
   res.status(200).send('Success');
 });
 
-app.get('/restart-nao-controller', (req, res) => {
+app.post('/restart-nao-controller', (req, res) => {
   console.warn('RESTARTING NAO CONTROLLER, WARNING: RECONNECTION REQUIRED.');
   nao = new nao_controller(nao_callback);
   res.status(200).send("NAOv5 Evolution Controller successfully restarted, view server console for details.");
 });
 
-app.get('/restart-yolo-controller', (req, res) => {
+app.post('/restart-yolo-controller', (req, res) => {
   console.warn('RESTARTING YOLO CONTROLLER.');
   yolo = new yolo_controller(yolo_callback);
   res.status(200).send("YOLOv8 Controller successfully restarted, view server console for details.");
@@ -35,9 +35,13 @@ app.post('/feed', (req, res) => {
   res.status(200).send("This is a stub route for \'/feed\'.")
 })
 
-app.get('/videos', (req, res) => {
+app.post('/videos', (req, res) => {
   res.status(200).send("This is a stub route for \'/videos\'.")
 });
+
+app.get('*', (req, res) => {
+  res.status(200).sendFile(__dirname + '\\drumpad.html');
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
