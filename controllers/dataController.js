2be283b7327e50c360316190947282d69a405ca0
Joel Leifer <jleifer@student.unimelb.edu.au>
2024-10-11 17:42:12 +1100

    updated controller routes

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,12 +1,44 @@
-const { nao_controller, yolo_controller, yolo_callback, nao_callback } = require('../Python/pythonHandler.js');
+const { nao_controller, cv_controller, cv_callback, nao_callback } = require('../Python/pythonHandler.js');
+const os = require('os');
+const fs = require('fs');
 var nao = new nao_controller(nao_callback);
-var yolo = new yolo_controller(yolo_callback);
+var cv = new cv_controller(yolo_callback);
 
 const computerVision = function(req, res)
 {
-    nao.feedLine('print("{}".format(str(get_camera_image())))')
+    nao.feedLine('get_frame');
     while (!nao.new_output);
-    let image_str = nao.getLastOutput();
+    let output = nao.getLastOutput();
+    if (output.includes('ERROR'))
+    {
+        console.error(`NAO ERROR: ${output}`);
+        return res.status(500).send(output);
+    }
+    let filename = nao.getLastOutput().split(':')[1];
+    cv.feedLine(`processImages([${os.pwd() + '/tempFileStorage/' + filename}])`);
+    while (!cv.new_output);
+    try
+    {
+        let detections = JSON.parse(cv.getLastOutput().split(':')[1]);
+    }
+    catch (err)
+    {
+        console.error(err);
+        res.status(500).send(err);
+    }
+    
+    console.log(detections);
+    console.warn(`Deleting Temporary File: ${os.pwd() + '/tempFileStorage/' + filename} . . .`);
+    fs.unlink(`${os.pwd() + '/tempFileStorage/' + filename}`);
+    console.warn('File Deleted.');
+
+    JSON.parse(detections);
+    let tts = ''
+    for (let i = 0; i < detections.length; i++)
+    {
+        tts += `There is a ${detections[i][0]} in the ${detections[i][1]} of my vision. `;
+    }
+    nao.feedLine(`tts \"${tts}\"`);
     res.status(200).send(`Success: Image Retrieved:\n${image_str}`);
 }
 
@@ -19,7 +51,17 @@ const listen = function(req, res)
 {
     try
     {
-        nao.feedLine("listen(10000)");
+        nao.feedLine("listen 10");
+        while (!nao.new_output);
+        if (output.includes('ERROR'))
+        {
+            console.error(`NAO ERROR: ${output}`);
+            return res.status(500).send(output);
+        }
+        let filename = nao.getLastOutput().split(':')[1];
+        
+
+
     }
     catch (e)
     {
@@ -37,9 +79,9 @@ const restartNaoController = function(req, res)
 
 const restartCVController = function(req, res)
 {
-    console.warn('RESTARTING YOLO CONTROLLER.');
-    yolo = new yolo_controller(yolo_callback);
-    res.status(200).send({message: "YOLOv8 Controller successfully restarted, view server console for details."});
+    console.warn('RESTARTING CV CONTROLLER.');
+    cv = new cv_controller(cv_callback);
+    res.status(200).send({message: "CV Controller successfully restarted, view server console for details."});
 }
 
 module.exports = { computerVision, translateAudio, listen, restartNaoController, restartCVController, nao };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 var { nao } = require('./dataController');
+var credientials = { user: null, expiry: Date.now() };
 
 const setUser = function(req, res)
 {
@@ -7,6 +8,10 @@ const setUser = function(req, res)
 
 const verifyUser = function(req, res)
 {
+    if (!credientials.user || Date.now() > credientials.expiry)
+    {
+        return res.status(403).send('Failed. invalid/expired credientials');
+    }
     res.status(200).send('Success');
 }
 
